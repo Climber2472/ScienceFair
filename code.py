8dfaeda73bf59caca251b4821da0a987ec424994
Rachel Hoffman <rachel@goofy.net>
2019-01-13 13:34:56 -0600

    Python3 Configuration

--- code.py
+++ code.py
@@ -12,26 +12,27 @@ patterns = {
 NUM_TRIAL = 25
 ###plant spaceing - optimal is 30 inches convert to cm
 PLANT_SPACING = 30 / 0.39370
-print PLANT_SPACING
+print (PLANT_SPACING)
 def createLayout(pattern):
     print("layout pattern %d" % (pattern))
+    return "hello"
 
 print("Choose a Pattern - ")
 for i in patterns:
     print("%d: %s" % (i, patterns[i]))
 
-    desired_pattern = input("Choose a Pattern number - ")
+desired_pattern = input("Choose a Pattern number - ")
 
 ##Choose Trials
 
 
 ### lay out the plants
 
-master_layout = createLayout()
+master_layout = createLayout(desired_pattern)
 
 ### loop NUM_TRIALS
 
-for t in xrange(NUM_TRIAL):
+for t in range(NUM_TRIAL):
     print("Starting trial %d..." % (t+1))
 
     ### make copy of layout
